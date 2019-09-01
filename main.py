30d484b29d3719f296ea8664e6df6a14b9fe910b
Kahfi <azanjabiil@gmail.com>
2019-09-01 19:38:32 +0700

    Yeay jadi

---
+++ main.py
@@ -0,0 +1,19 @@
+print('kahfi ganteng :)')
+
+
+a = 100
+b = 200
+c = 70
+
+print(a, b, c)
+
+# gunakan satu bilangan untuk dicek, misal mulai dari a
+
+if a > b and a > c:
+    print('a terbesar, nilainya', a)
+
+if b > a and b > c:
+    print('b terbesar, nilainya', b)
+
+if c > a and c > b:
+    print('c terbesar, nilainya', c)
