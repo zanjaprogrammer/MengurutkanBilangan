c8628023f31df4a26c14d0db85f8180e179d34da
Kahfi <azanjabiil@gmail.com>
2019-09-01 19:56:28 +0700

    Latihan 2

---
+++ cek_dua_bilangan.py
@@ -0,0 +1,15 @@
+a = 1
+b = 1
+
+print(a, b)
+
+# gunakan satu bilangan untuk dicek, misal mulai dari a
+
+if a > b:
+    print("a terbesar, nilainya", a)
+
+if b > a:
+    print("b terbesar, nilainya", b)
+
+if a == b:
+    print("a dan b sama nilainya", a)
